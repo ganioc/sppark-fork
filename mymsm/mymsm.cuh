55ec4df568ec3e02f9920be6c90cd92d883ab03e
yango <o3.yang@hotmail.com>
2022-12-29 11:05:07 +0800

    update invoke start from here

--- mymsm/mymsm.cuh
+++ mymsm/mymsm.cuh
@@ -3,8 +3,38 @@
 #define __MYMSM_CUH__
 
 #include <cuda.h>
+#include <cassert>
+#include <vector>
 
+#include <util/exception.cuh>
 #include <util/rusterror.h>
+#include <util/gpu_t.cuh>
+
+#include <stdio.h>
+
+RustError invoke(
+    point_t&         out,
+    const affine_t*  points_,
+    size_t           npoints,
+    const scalar_t*  scalars,
+    bool             mont =  true,
+    size_t           ffi_affine_sz = sizeof(affine_t)
+){
+    printf("invoke()\n");
+    printf("npoints: %ld\n", npoints);
+
+
+    return RustError{cudaSuccess};
+}
+RustError invoke(
+    point_t&        out,
+    vec_t<affine_t> points,
+    const scalar_t* scalars,
+    bool            mont = true,
+    size_t          ffi_affine_sz = sizeof(affine_t))
+{
+    return invoke(out, points, points.size(), scalars, mont, ffi_affine_sz);
+}
 
 template<class bucket_t, class point_t, class affine_t, class scalar_t> 
 static RustError mymsm_pippenger(
@@ -16,7 +46,15 @@ static RustError mymsm_pippenger(
     size_t         ffi_affine_sz = sizeof(affine_t)   
 )
 {
-    return RustError{1};
+    printf("mymsm_pippenger()\r\n");
+    // *out.add( (point_t) points[0] );
+    return invoke(*out, 
+                vec_t<affine_t>{points, npoints},
+                scalars,
+                mont,
+                ffi_affine_sz
+            );
+    // return RustError{cudaSuccess};
 }
 
 
