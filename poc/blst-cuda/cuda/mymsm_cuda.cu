5e2af0adbc630851f937e16002183bdfadc2a754
yango <o3.yang@hotmail.com>
2023-01-12 17:48:07 +0800

    update point

--- poc/blst-cuda/cuda/mymsm_cuda.cu
+++ poc/blst-cuda/cuda/mymsm_cuda.cu
@@ -14,21 +14,21 @@ typedef xyzz_t<fp_t> bucket_t;
 typedef bucket_t::affine_inf_t affine_t;
 typedef fr_t scalar_t;
 
-#include <mymsm/mymsm.cuh>
+// #include <mymsm/mymsm.cuh>
 #include <mymsm/polynomial.cuh>
 
 
 #ifndef __CUDA_ARCH__
 extern "C" {
-    RustError mymsm_pippenger_inf(
-        point_t*        out,
-        const affine_t  points[],
-        size_t          npoints,
-        const scalar_t  scalars[],
-        size_t          ffi_affine_sz
-    ){
-        return mymsm_pippenger<bucket_t>(out, points, npoints, scalars, false, ffi_affine_sz);
-    }
+    // RustError mymsm_pippenger_inf(
+    //     point_t*        out,
+    //     const affine_t  points[],
+    //     size_t          npoints,
+    //     const scalar_t  scalars[],
+    //     size_t          ffi_affine_sz
+    // ){
+    //     return mymsm_pippenger<bucket_t>(out, points, npoints, scalars, false, ffi_affine_sz);
+    // }
 
     RustError mymsm_polynomial_inf(size_t degree){
         return mymsm_polynomial(degree);
